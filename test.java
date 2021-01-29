7909762f421716324402ad969ff5f61c0a8843a3
ksak54 <kumarsaksham54@gmail.com>
2021-01-29 17:25:07 +0530

    Last Commit

---
+++ test.java
@@ -0,0 +1,8 @@
+public class test {
+    public static void main(String[] args) {
+        String h = "haha";
+        String m = new String("haha");
+        String b = h + m;
+        System.out.println(b);
+    }
+}
